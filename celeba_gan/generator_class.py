46671ee74364ce595495012e92fc5242488a8af7
MarioAuditore <57262352+MarioAuditore@users.noreply.github.com>
2023-12-21 18:45:49 +0300

    Add files via upload

---
+++ celeba_gan/generator_class.py
@@ -0,0 +1,94 @@
+import torch
+import torch.nn as nn
+
+
+class Generator(nn.Module):
+    """DCGAN Generator G(z)"""
+
+    def __init__(self, latent_dim=100):
+        super(Generator, self).__init__()
+
+        # Project and reshape
+        self.linear = nn.Sequential(
+            nn.Linear(latent_dim, 512 * 4 * 4, bias=False),
+            nn.BatchNorm1d(512 * 4 * 4),
+            nn.ReLU(inplace=True))
+
+        # Upsample
+        self.features = nn.Sequential(
+            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(64, 3, 4, 2, 1, bias=False),
+            nn.Tanh())
+        """
+        self.features = nn.Sequential(
+            nn.ConvTranspose2d(512, 256, kernel_size=5, stride=2, padding=2,
+                output_padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(256, 128, 5, 2, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(128, 64, 5, 2, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(64, 3, 5, 2, 2, 1, bias=False),
+            nn.Tanh())
+        """
+
+    def forward(self, x):
+        x = self.linear(x).view(x.size(0), -1, 4, 4)
+        return self.features(x)
+
+
+class Discriminator(nn.Module):
+    """DCGAN Discriminator D(z)"""
+
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, 128, 4, 2, 1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(128, 256, 4, 2, 1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(256, 512, 4, 2, 1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(512, 1, 4, 1))
+        """
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=5, stride=2, padding=2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, 128, 5, 2, 2),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(128, 256, 5, 2, 2),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(256, 512, 5, 2, 2),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(512, 1, 4, bias=False))
+        """
+
+    def forward(self, x):
+        return self.features(x).view(-1)
+
+
+    def clip(self, c=0.05):
+        """Weight clipping in (-c, c)"""
+
+        for p in self.parameters():
+            p.data.clamp_(-c, c)
